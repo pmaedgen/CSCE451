2cd0d54252f612b98d46a530968ed2e5323e5345
Patrick Maedgen <57485900+pmaedgen@users.noreply.github.com>
2022-02-25 13:47:58 -0600

    compute rectangle

--- program.cpp
+++ program.cpp
@@ -23,20 +23,25 @@ bool myRandom(){
     return false;
 }
 
+int compute_area(unsigned int length, unsigned int width) {
+  int area = length * width;
+  return area;
+}
+
 
 int main() {
     srand (time(NULL));
     cout<< "Your inputs will be added together :)";
     int a,b,c;
     cout<< "Enter an Int: ";
-    cin>>a; 
+    cin>>a;
     cout<< "Enter an Int: ";
     cin>>b;
     cout<< "Enter an Int: ";
     cin>>c;
 
     bool ez = myRandom();
-    cout<<"Funny Face:"
+    cout<<"Funny Face:";
     cout<<":"<<ez<<endl;
 
 
@@ -44,14 +49,30 @@ int main() {
         cout<<a+b+c<<endl;
     }
     else{
-       cout<<" "<<printNum(add(a,b,c));
+       cout<<" "<<printNum(add(a,b,c)) << endl;
     }
     // printf() displays the string inside quotation
+
+
+    cout << "\nFind the area of a rectangle given its dimensions ;)" << endl;
+    unsigned int l, w;
+    int area;
+
+    cout << "Enter an length: ";
+    cin >> l;
+    cout << "Enter an width: ";
+    cin >> w;
+
+    ez = myRandom();
+
+    if(ez) {
+      w = w << 5;
+    }
+
+    area = compute_area(l, w);
+
+    cout << "The area of the rectangle is: " << area << endl;
+
+
     return 0;
 }
-
-
-
-
-
-
